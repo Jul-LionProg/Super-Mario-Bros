9fc1c2d90c18d5c5b3b79d99faaddb9ee33fec97
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-03-13 09:34:03 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
+const mario = document.querySelector('.mario')
+const pipe = document.querySelector('.pipe')
+
+const start = document.querySelector('.start')
+const gameOver = document.querySelector('.game-over')
+
+audioStart = new Audio('./src/audio/audio_theme.mp3')
+audioGameOver = new Audio('./src/audio/audio_gameover.mp3')
+
 
