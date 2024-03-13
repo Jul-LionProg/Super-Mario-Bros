b6a698e7a65df619d2ecda757f978c6bb5ebba80
Julio Felipe Canabarro <108811948+Jul-LionProg@users.noreply.github.com>
2024-03-13 09:34:15 -0300

    Update script.js

--- script.js
+++ script.js
@@ -8,3 +8,10 @@ audioStart = new Audio('./src/audio/audio_theme.mp3')
 audioGameOver = new Audio('./src/audio/audio_gameover.mp3')
 
 
+const startGame = () => {
+  pipe.classList.add('pipe-animation')
+  start.style.display = 'none'
+
+  // audio
+  audioStart.play()
+}
